836bfc51bca53fcb28cc21391cefd0498146eaa1
Ivan <49728539+KaminoontyoSenpai2000@users.noreply.github.com>
2019-07-06 21:10:20 +0300

    Add files via upload

---
+++ JS July 06.07.2019/Today learnings JS/main.js
@@ -0,0 +1,43 @@
+//var variable1 = "Hello world";
+
+//alert(variable1);
+
+//var a = 4;
+//var b = 5;
+ 
+//if (a === b) {
+//	alert('A equal 8');
+//}
+//else {
+//	alert('A doesn\'t equal 8');
+//}
+
+
+
+//var a=3;
+//var b=2;
+
+//function dely(number) {
+//	return number%2;
+//}
+
+//if (dely (a) === 0) {
+//	alert('true')
+//}
+//else {
+//	alert('false')
+//}
+
+//if (dely (b) === 0) {
+//	alert('true')
+//}
+//else {
+//	alert('false')
+//}
+
+//var array =[];
+
+//var colors = ["red", "green", "blue"];
+
+//var redColor = colors[0];
+
